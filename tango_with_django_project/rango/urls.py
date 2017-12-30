65baf28b1ea917d36146ea9103d91eddf1f92524
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2017-12-30 21:06:33 +0000

    Failed attempt to run in non-debug mode
    I vaguely remember this topic being touched on in one of the
    courses I have watched. However, here I am failing to fix the
    problem. When running with DEBUG=FALSE, no amount of additional
    configuration allows for the image to be shown. Indeed, in
    debug mode, the 'static' folder is handled/served by the light
    weight web-server. But not in 'standard' mode.
    
    I have found snippets online of stuff I can add to the URLPATTERNS
    array. However, everyone says this is not efficient. I will put
    this aside for now. There is a point where this should be
    tested/ran on a some online platform. Hopefully, it will work
    just fine then.
    
        --- Lionel Saliou, Ph.D

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^about', views.about, name='about'),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, 
+        document_root=settings.STATIC_ROOT)
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c)sn68&3k!38cxfc2uxr9xzf$2wc6&xs02k&!)8qg09%1!oy8b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
