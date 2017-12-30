ac948741b1da64a31efe2b089ad76d28b689de5a
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2017-12-30 21:52:22 +0000

    Fixed Media folder and success getting GIF file
    I had to look at the Digging into Django code from code school
    in order to make it work. As shown in this diff, I also had
    to move the media folder to the root of the project.
    
    On the one hand it makes sense. The application, such as Rango,
    itself should not care where the 'media' are and they certainly
    should not be tracked as part of the development.
    
       --- Lionel Saliou, Ph.D
    
    Edit: forgot to delete the commented out line.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -14,4 +15,7 @@ if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, 
         document_root=settings.STATIC_ROOT)
 else:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [ re_path(r'^media/(?P<path>.*)$', 
+                       serve, 
+                       {'document_root': settings.MEDIA_ROOT,})
+    ]
