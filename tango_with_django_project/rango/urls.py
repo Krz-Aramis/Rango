960b1428677fd3710fa691d6a863e0cc36425a18
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2017-12-31 11:35:19 +0000

    Moved MEDIA and STATIC conf to root URLs.py
    It is not the responsibility of our application to specify and/or
    manage the manner in which MEDIA and STATIC content is served;
    debug mode or not.
    
    In this commit, we move this code to the root configuration where
    it belongs. Along with this, we correct the mistake of trying
    to specify the "application" name as part of the path of files
    in the media directory.
    
    A quick test showed that static content is still being displayed
    correctly and that items from media also appear provided the
    URL is correct.
    
         --- Lionel Saliou, Ph.D

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,21 +1,8 @@
 from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^about', views.about, name='about'),
 ]
-
-# TODO: remove this BEFORE deployment. It is not efficient or recommended.
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, 
-        document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += [ re_path(r'^media/(?P<path>.*)$', 
-                       serve, 
-                       {'document_root': settings.MEDIA_ROOT,})
-    ]
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -14,11 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.views.static import serve
 from rango import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
 ]
+
+# TODO: remove this BEFORE deployment. It is not efficient or recommended.
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, 
+        document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [ re_path(r'^media/(?P<path>.*)$', 
+                       serve, 
+                       {'document_root': settings.MEDIA_ROOT,})
+    ]
