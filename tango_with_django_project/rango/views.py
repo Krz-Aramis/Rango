1d5e64a3b8660fe59d8a4d1e0c15f02cd319090d
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-01-02 06:52:17 +0000

    Display 'top categories' in order on Rango page
    In this commit, we query the database for data related to our
    Category model. Using the OO aspect of python, in one statement
    we get all of the categories stored so far and place the top
    5 in our context supplied to the template context.
    
    In the template, we iterate throught the list (provided it is
    valid) and display the results.
    
        --- Lionel Saliou, Ph.D

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    # Our requirements is to show the top 5 categories.
+    # Therefore, we poll of the categories arranged by number
+    # of likes and supply the first 5 to our template for display
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'boldmessage': "I am bold font from the context",
+                    'categories' : category_list
+    }
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
