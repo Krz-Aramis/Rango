bca54836d861705ecfdf0657e5eb7bf56e26bab3
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-01-02 07:07:47 +0000

    Added Slugfield to Category model
    I understand the principle behind this. We want the URLs to be
    human readable. However, I am not too sure when to it arise
    during the design that this technique would be a solution.
    
    I admit that the application I wish to develop next is quite
    hazy... so no panic :-)
    
        --- Lionel Saliou, Ph.D

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+# Class to customise the Admin interface
+# It ensures that the slug field is automatically populated
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug':('name',)}
+
+# Update the registration of our models with our custom interface
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name    = models.CharField(max_length=128, unique=True)
     views   = models.IntegerField(default=0)
     likes   = models.IntegerField(default=0)
+    slug    = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        # Uncomment if slug must remain the same regardless of the category's name
+        # if self.id is None:
+        #   self.slug = slugify(self.name)
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "Categories"
