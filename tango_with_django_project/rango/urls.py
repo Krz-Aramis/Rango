36ba6fcc3ceedc09f79d0b00563f0f007daf58a6
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-01-05 07:52:02 +0000

    Use namespace in template URL
    As noted in the Django documentation, it could very well be that
    'indexes' or 'details' pages exist for every "app" that compose
    a site. Therefore, there must be a way of mapping this correctly.
    Here we use the "rango" namespace.
    
    The server complained that it was not a unique namespace because
    we imported it twice in the "project directory". I simplified
    this URLs.py however, I had to expand Rango's because otherwise
    some of the redirection (such as from the login page) would fail.
    
         --- Lionel Saliou, Ph.D

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, re_path
 from django.conf.urls import url
 from . import views
 
+app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^rango/$', views.index, name='index'),
     re_path(r'category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     re_path(r'about', views.about, name='about'),
     re_path(r'^add_category/$', views.add_category, name='add_category'),
