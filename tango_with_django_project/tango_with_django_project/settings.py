645aa3288fceb3a9bfd70825eafa9b8c6e8ec290
Dr. Lionel Saliou <lionel.saliou@gmail.com>
2018-01-07 20:39:53 +0000

    FIXED password change page
    When defining the TEMPLATE_DIR variable, it is possible to
    force the display of our page provided that:
    a) In TEMPLATES, we set the DIRS key to [TEMPLATES, ]
    b) we create a templates/regitration folder in the
    tango_with_django_project directory
    
    In my view, this is bad. Indeed, the APP_DIRS is set to True, so
    the framework should search for templates in our application
    directories too! Right now, there is only 1 application, Rango.
    However, it has templates for both itself and registration.
    
    It does not make sense that Login and Registration for example
    are found correctly.
    
    EDIT: issue fixed
    
    This was resolved by ensuring that RANGO is the first application
    loaded. The template engine probably looks-up stuff in order.
    Because the layout of the page was obviously inspired by the
    style of the admin app, this suggested that there already is
    a corresponding file. With Rango loaded first, the engine
    finds the files we created. This is perhaps why the django-auth-redux
    project suggests loading 'registration' before 'auth'. However
    for us, it broke the application. Also, I checked and we are following
    the recommended approach with template definition and storage. So, there
    might still be bugs within the django-auth-redux application.
    
        --- Lionel Saliou, Ph.D

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    'rango',
     'django.contrib.admin',
+    'registration',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango',
-    'registration',
+
 ]
 
 MIDDLEWARE = [
